a31b13f96433d7f56d024ffe11d4b9c4dc693c47
Мария Гмыль <maniatwoia@gmail.com>
2023-07-19 14:38:41 +0300

    commit4

---
+++ Seminar 6/HWTask 1/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Введите количество чисел (M): ");
+        int M = int.Parse(Console.ReadLine());
+
+        int count = 0;
+        for (int i = 0; i < M; i++)
+        {
+            Console.Write("Введите число: ");
+            int number = int.Parse(Console.ReadLine());
+            if (number > 0)
+            {
+                count++;
+            }
+        }
+
+        Console.WriteLine("Количество чисел больше нуля: " + count);
+    }
+}
---
+++ Seminar 6/HWTask 2/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Введите значение k1: ");
+        double k1 = Convert.ToDouble(Console.ReadLine());
+
+        Console.Write("Введите значение b1: ");
+        double b1 = Convert.ToDouble(Console.ReadLine());
+
+        Console.Write("Введите значение k2: ");
+        double k2 = Convert.ToDouble(Console.ReadLine());
+
+        Console.Write("Введите значение b2: ");
+        double b2 = Convert.ToDouble(Console.ReadLine());
+
+        if (k1 == k2)
+        {
+            if (b1 == b2)
+            {
+                Console.WriteLine("Уравнения совпадают, пересечения бесконечно много");
+            }
+            else
+            {
+                Console.WriteLine("Уравнения параллельны, пересечений нет");
+            }
+        }
+        else
+        {
+            double x = (b2 - b1) / (k1 - k2);
+            double y = k1 * x + b1;
+            Console.WriteLine($"Точка пересечения: ({x}, {y})");
+        }
+    }
+}
